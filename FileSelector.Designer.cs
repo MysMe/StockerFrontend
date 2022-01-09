5fd3d54e18c927bb7721cced55034ca4fba2e1b7
Adam <adamgiraudet@gmail.com>
2022-01-09 21:26:26 +0000

    Add project files.

---
+++ FileSelector.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace StockerFrontend
+{
+    partial class FileSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectCountButton = new System.Windows.Forms.Button();
+            this.SelectStockFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectCountButton
+            // 
+            this.SelectCountButton.Location = new System.Drawing.Point(12, 12);
+            this.SelectCountButton.Name = "SelectCountButton";
+            this.SelectCountButton.Size = new System.Drawing.Size(83, 41);
+            this.SelectCountButton.TabIndex = 0;
+            this.SelectCountButton.Text = "Select Count File";
+            this.SelectCountButton.UseVisualStyleBackColor = true;
+            this.SelectCountButton.Click += new System.EventHandler(this.SelectCountButton_Click);
+            // 
+            // SelectStockFile
+            // 
+            this.SelectStockFile.Location = new System.Drawing.Point(101, 12);
+            this.SelectStockFile.Name = "SelectStockFile";
+            this.SelectStockFile.Size = new System.Drawing.Size(93, 41);
+            this.SelectStockFile.TabIndex = 1;
+            this.SelectStockFile.Text = "Select Stock File";
+            this.SelectStockFile.UseVisualStyleBackColor = true;
+            this.SelectStockFile.Click += new System.EventHandler(this.SelectStockFile_Click);
+            // 
+            // FileSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SelectStockFile);
+            this.Controls.Add(this.SelectCountButton);
+            this.Name = "FileSelector";
+            this.Text = "FileSelector";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectCountButton;
+        private System.Windows.Forms.Button SelectStockFile;
+    }
+}
---
+++ FileSelector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockerFrontend
+{
+    public partial class FileSelector : Form
+    {
+        public FileSelector()
+        {
+            InitializeComponent();
+        }
+
+        private static string? getFile()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                return openFileDialog1.FileName;
+            }
+            return null;
+        }
+
+        private void SelectCountButton_Click(object sender, EventArgs e)
+        {
+            string? file = getFile();
+        }
+
+        private void SelectStockFile_Click(object sender, EventArgs e)
+        {
+            string? file = getFile();
+        }
+    }
+}
