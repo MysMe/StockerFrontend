ba4c1a3703a6ccf36d42e3ea6b6250997fb45884
Adam Giraudet <adamgiraudet@gmail.com>
2022-03-08 16:49:44 +0000

    Designed basic file reciever as a proof of concept

--- Forms/FileSelector.cs
+++ Forms/FileSelector.cs
@@ -1,5 +1,7 @@
 ﻿using StockerFrontend.Forms;
+using StockerFrontend.Modals.Global;
 using StockerFrontend.Natives;
+using StockerFrontend.Natives.LanCon;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -23,7 +25,80 @@ namespace StockerFrontend
             this.unified = unified;
             this.count = count;
             InitializeComponent();
-            DialogResult = DialogResult.Cancel;
+
+            UDPListen form = new UDPListen();
+            form.Show();
+        }
+
+        private static void LanConTest()
+        {
+            UDPReceiver UDP = new UDPReceiver(40404);
+            UDPMessage? UDPM = null;
+            while (true)
+            {
+                UDPM = UDP.AwaitMessage(500);
+                if (UDPM != null)
+                {
+                    if (UDPM.GetRequest() == UDPRequest.requestAddress)
+                    {
+                        Console.Write("!");
+                    }
+                    else if (UDPM.GetRequest() == UDPRequest.requestLink)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("?");
+                    }
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine(UDPM.GetAddress().ToString() + " would like to open a link, accept? [Y/N]");
+            char key = '\0';
+            do
+            {
+                key = (char)Console.Read();
+            } while (key != 'Y' && key != 'N');
+            if (key == 'N')
+            {
+                UDP.Respond(UDPM, UDPRequest.denyLink);
+                Console.WriteLine("Connection denied.");
+                return;
+            }
+
+            Console.WriteLine("Accepted link, awaiting connection.");
+
+            TCPReceiver TCP = new TCPReceiver(UDPM.GetAdditional());
+
+            TCPReceiverContent? content = null;
+
+
+            UDP.Respond(UDPM, UDPRequest.acceptLink);
+            while ((content = TCP.AwaitAccept(1000)) == null)
+            {
+                Console.Write(".");
+                UDP.Respond(UDPM, UDPRequest.acceptLink);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine(content.ToString() + " connected.");
+
+            content = null;
+            while ((content = TCP.AwaitMessage(1000)) == null)
+            {
+                Console.Write(".");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Received: ");
+            Console.WriteLine(content.ToString());
+            return;
         }
 
         private static string? getFile()
